994e7b876c73a3b947b3ed2001b5b78a9f585fb6
Daniel Molina <daniel.molina@metaturf.com>
2022-05-16 16:54:24 +0200

    ZKU-Cohort 3 Week 2 Assignment

--- Part1/test/merkle-test.js
+++ Part1/test/merkle-test.js
@@ -47,7 +47,7 @@ describe("MerkleTree", function () {
     it("Insert two new leaves and verify the first leaf in an inclusion proof", async function () {
         await merkleTree.insertLeaf(1);
         await merkleTree.insertLeaf(2);
-
+        
         const node9 = (await merkleTree.hashes(9)).toString();
         const node13 = (await merkleTree.hashes(13)).toString();
 
@@ -63,14 +63,42 @@ describe("MerkleTree", function () {
         const calldata = await groth16.exportSolidityCallData(editedProof, editedPublicSignals);
     
         const argv = calldata.replace(/["[\]\s]/g, "").split(',').map(x => BigInt(x).toString());
+
+        const a = [argv[0], argv[1]];
+        const b = [[argv[2], argv[3]], [argv[4], argv[5]]];
+        const c = [argv[6], argv[7]];
+        const input = argv.slice(8);  //Input is the root hash
     
+        expect(await merkleTree.verify(a, b, c, input)).to.be.true;
+    });
+
+    // [bonus] verify the second leaf with the inclusion proof
+    it("Verify the second leaf in an inclusion proof", async function () {
+
+        await merkleTree.insertLeaf(1);
+        await merkleTree.insertLeaf(2);
+
+        const node9 = (await merkleTree.hashes(9)).toString();
+        const node13 = (await merkleTree.hashes(13)).toString();
+
+        const Input = {
+            "leaf": "2",
+            "path_elements": ["1", node9, node13],
+            "path_index": ["1", "0", "0"]
+        }
+        const { proof, publicSignals } = await groth16.fullProve(Input, "circuits/circuit_js/circuit.wasm","circuits/circuit_final.zkey");
+
+        const editedPublicSignals = unstringifyBigInts(publicSignals);
+        const editedProof = unstringifyBigInts(proof);
+        const calldata = await groth16.exportSolidityCallData(editedProof, editedPublicSignals);
+    
+        const argv = calldata.replace(/["[\]\s]/g, "").split(',').map(x => BigInt(x).toString());
+            
         const a = [argv[0], argv[1]];
         const b = [[argv[2], argv[3]], [argv[4], argv[5]]];
         const c = [argv[6], argv[7]];
         const input = argv.slice(8);
 
         expect(await merkleTree.verify(a, b, c, input)).to.be.true;
-
-        // [bonus] verify the second leaf with the inclusion proof
     });
 });
